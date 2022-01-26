8651ca5661579a851555f85393a90510b1914a70
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-01-26 16:06:01 -0300

    play() function implemented

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function computerPlay() {
   let randomIndex = Math.floor(Math.random() * computerChoices.length);
   return computerChoices[randomIndex];
 }
+
+function play(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    return "TIE!";
+  } else if ((playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors")  || (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") || (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper")) {
+    return `You Win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`;
+  } else {
+    return `You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`;
+  }
+}
